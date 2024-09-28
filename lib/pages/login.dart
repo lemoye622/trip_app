a3aa69a98041a719a30dd6ffe54da930dbbb3606
lemoye <2557418823@qq.com>
2024-09-28 19:46:53 +0800

    feat: 实现登录页面交互&跳转H5注册页面

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:trip_app/utils/string_util.dart';
+import 'package:trip_app/utils/view_util.dart';
+import 'package:trip_app/widgets/input_widget.dart';
+import 'package:trip_app/widgets/login_button_widget.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -16,7 +21,7 @@ class _LoginState extends State<Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [..._background()],
+        children: [..._background(), _content()],
       ),
     );
   }
@@ -30,5 +35,64 @@ class _LoginState extends State<Login> {
         decoration: const BoxDecoration(color: Colors.black54),
       ))
     ];
+  }
+
+  _content() {
+    return Positioned.fill(
+        left: 25,
+        right: 25,
+        //    为了适配不同的手机页面，使用 ListView 使窄屏手机页面可以上下滚动
+        child: ListView(children: [
+          hiSpace(height: 100),
+          const Text(
+            '账号密码登录',
+            style: TextStyle(
+                fontSize: 26, color: Colors.white, fontWeight: FontWeight.w400),
+          ),
+          hiSpace(height: 40),
+          InputWidget('请输入账号', onChanged: (text) {
+            userName = text;
+            _checkInput();
+          }),
+          hiSpace(height: 10),
+          InputWidget('请输入密码', isObscureText: true, onChanged: (text) {
+            password = text;
+            _checkInput();
+          }),
+          hiSpace(height: 45),
+          LoginButtonWidget('登录',
+              enable: loginEnable, onPressed: () => _login()),
+          hiSpace(height: 15),
+          Align(
+              alignment: Alignment.centerRight,
+              child: InkWell(
+                child:
+                    const Text('注册账号', style: TextStyle(color: Colors.white)),
+                onTap: () => _jumpRegistration(),
+              ))
+        ]));
+  }
+
+  void _checkInput() {
+    bool enable;
+    if (isNotEmpty(userName) && isNotEmpty(password)) {
+      enable = true;
+    } else {
+      enable = false;
+    }
+    setState(() {
+      loginEnable = enable;
+    });
+  }
+
+  _login() {}
+
+  Future<void> _jumpRegistration() async {
+    // 跳转到注册账号H5页面
+    Uri url = Uri.parse(
+        'https://juejin.cn/post/7398342795435982902');
+    if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
+      throw Exception('Could not launch $url');
+    }
   }
 }
